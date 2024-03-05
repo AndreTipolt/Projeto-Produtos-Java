b9908c208d3f19c8c659df0054c5ad9775389522
andretipolt <andretipoltlopes@gmail.com>
2024-03-05 17:50:36 -0300

    Add Class Vetor

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import classes.Vetor;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
 
+        Vetor vetor = new Vetor(4);
+
+        try {
+
+            vetor.adiciona(123);
+            vetor.adiciona("Banana banana");
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println(vetor);
     }
 }
